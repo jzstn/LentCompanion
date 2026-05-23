5963376ded3474445ca2310d028269685670ab59
jzstn <justinvarghese15@hotmail.com>
2026-05-22 23:43:35 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const EVENT = {
 
 // Add your image at: public/invitation.png
 const INVITATION_IMAGE_URL = '/invitation.png'
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzFdxXeMi8B9WAxC51ykGxTDbB3-7c5OCgvW-h316QJENjCxhV9yXla4StYlaLkoogN/exec'
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzLEy_dQ2PJbYal32ZV-3pGMF87S4d-NRIOipOBBDnvraMVww2JZYkClevsDM8Zr0_O/exec'
 
 export default function App() {
     const [inviteCode, setInviteCode] = useState('')
